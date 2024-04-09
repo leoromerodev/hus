3daa04ce58bcb4d9bec592e5733fed792a658ee2
Leonardo Romero Castro <leoromerodev@outlook.com>
2024-04-09 17:40:42 -0500

    Update scriptGetAllBranchesGit.ps1

--- poweshell-scripts/scriptGetAllBranchesGit.ps1
+++ poweshell-scripts/scriptGetAllBranchesGit.ps1
@@ -1,13 +1,18 @@
-# Definir las ramas a excluir
-$excluirRama1 = "DEV"
-$excluirRama2 = "release/COB"
-$excluirRama3 = "SIT"
-$excluirRama4 = "release/UAT"
-$excluirRama5 = "master"
+# Directorio donde se guardará el archivo CSV
+$directorio = "C:\ramas"
+
+# Verificar si el directorio existe, si no existe, crearlo
+if (-not (Test-Path $directorio)) {
+    New-Item -ItemType Directory -Path $directorio | Out-Null
+}
+
+# Nombre del archivo CSV con formato AAAAMMDDhhmmss.csv
+$nombreArchivo = "ramas_" + (Get-Date).ToString("yyyyMMddHHmmss") + ".csv"
+$rutaCompleta = Join-Path -Path $directorio -ChildPath $nombreArchivo
 
 # Imprimir encabezado
-Write-Output "Contador | Fecha de Creación | Último Commit | Autor | Descripción | Nombre de la Rama | Rama Base"
-Write-Output "--------------------------------------------------------------------------------------------------------"
+$encabezado = "Contador, Fecha de Creación, Último Commit, Autor, Descripción, Nombre de la Rama, Rama Base"
+$encabezado | Out-File -FilePath $rutaCompleta -Encoding UTF8
 
 # Obtener todas las ramas locales y remotas, excluyendo las especificadas
 $ramas = git branch --all --format="%(refname:short)" | ForEach-Object { $_.Trim() } | Where-Object { $_ -notmatch "^($excluirRama1|$excluirRama2|$excluirRama3|$excluirRama4|$excluirRama5)$" }
@@ -33,11 +38,14 @@ foreach ($rama in $ramas) {
     # Obtener la rama base, ajustar según la necesidad de cálculo de rama base
     $ramaBase = git merge-base master $rama
 
-    # Imprimir el contador y luego la información recolectada
-    Write-Output "$contador | $fechaCreacion | $ultimoCommitFecha | $ultimoCommitAutor | $ultimoCommitDescripcion | $rama | $ramaBase"
+    # Construir la línea CSV
+    $linea = "$contador, $fechaCreacion, $ultimoCommitFecha, `"$ultimoCommitAutor`", `"$ultimoCommitDescripcion`", `"$rama`", `"$ramaBase`""
+
+    # Añadir la línea al archivo CSV
+    $linea | Out-File -FilePath $rutaCompleta -Encoding UTF8 -Append
 
     # Incrementar el contador
     $contador++
 }
 
-Write-Output "--------------------------------------------------------------------------------------------------------"
+Write-Output "El archivo CSV se ha guardado en: $rutaCompleta"
